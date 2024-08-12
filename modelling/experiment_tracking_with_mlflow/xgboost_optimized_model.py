b7de3d32908b42128c4f162e88f2910e16f276de
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-08-12 20:58:07 +0300

    modelling experiment_tracking_with_mlflow

--- modelling/experiment_tracking_with_mlflow/xgboost_optimized_model.py
+++ modelling/experiment_tracking_with_mlflow/xgboost_optimized_model.py
@@ -5,7 +5,7 @@ import mlflow
 import mlflow.xgboost
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, r2_score, root_mean_squared_error
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 import pandas as pd
 
 
@@ -47,7 +47,7 @@ with mlflow.start_run():
     y_pred = model.predict(X_test)
 
     # Calculate metrics
-    rmse = root_mean_squared_error(y_test, y_pred)
+    rmse = mean_squared_error(y_test, y_pred, squared=False)
     mae = mean_absolute_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
 
